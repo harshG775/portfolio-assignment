835cb83f467e44485baf46097071ecd851a0d31d
Harsh Gaur <hgaur491@gmail.com>
2025-03-04 22:58:55 +0530

    SkillsBanner

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -1,16 +1,39 @@
 import { ArrowUpRightIcon } from "lucide-react";
 import { Link } from "react-router";
 
+const skillsBanner = [{ title: "WEBFLOW" }, { title: "FIGMA" }, { title: "DESIGNER" }, { title: "DEVELOPER" }];
+
+function SkillsBanner() {
+    return (
+        <div className="w-full py-8 mb-10 inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+            <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+                {skillsBanner.map(({ title, img }, i) => (
+                    <div key={i} className="mx-8 flex items-center gap-2">
+                        <img src={img} alt={title} className="w-10 h-10 object-contain" />
+                        <span className="text-lg font-semibold text-secondary-foreground ">{title}</span>
+                    </div>
+                ))}
+            </ul>
+            <ul
+                className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+                aria-hidden="true"
+            >
+                {skillsBanner.map(({ title, img }, i) => (
+                    <div key={i} className="mx-8 flex items-center gap-2">
+                        <img src={img} alt={title} className="w-10 h-10 object-contain" />
+                        <span className="text-lg font-semibold text-secondary-foreground ">{title}</span>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
 export default function Footer() {
     return (
-        <footer className="w-full border-t bg-background py-6">
-            <div className="container mx-auto mb-6">
-                <div className="flex overflow-x-auto mb-6">
-                    <div className="p-20">FIGMA</div>
-                    <div className="p-20">FIGMA</div>
-                    <div className="p-20">FIGMA</div>
-                    <div className="p-20">FIGMA</div>
-                </div>
+        <footer className="w-full  bg-background py-6">
+            <div className="container mx-auto mb-10">
+                <SkillsBanner />
                 <div>
                     <h2 className="text-4xl font-bold text-center mb-6">LET’S TALK!</h2>
                     <div className="flex justify-center">
