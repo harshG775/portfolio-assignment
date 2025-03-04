66d0bbad650a469ddf16b2f4cd00d9e26e159192
Harsh Gaur <hgaur491@gmail.com>
2025-03-04 21:31:30 +0530

    Section_Experience

--- src/components/page/home/Section_Experience.jsx
+++ src/components/page/home/Section_Experience.jsx
@@ -1,8 +1,49 @@
 import Section from "@/components/partials/Section";
+
+const experiences = [
+    {
+        title: "Lead Product Designer",
+        company: "Fortknox",
+        startDate: "Mar 2022",
+        endDate: "Oct 2023",
+    },
+    {
+        title: "Intern Designer",
+        company: "OmniSafe",
+        startDate: "Mar 2022",
+        endDate: "Oct 2023",
+    },
+    {
+        title: "UI Designer",
+        company: "Doradesign",
+        startDate: "Mar 2022",
+        endDate: "Oct 2023",
+    },
+    {
+        title: "Frontend Developer",
+        company: "OpacityAuthor",
+        startDate: "Mar 2022",
+        endDate: "Oct 2023",
+    },
+];
+
 export default function Section_Experience() {
     return (
         <Section title="Experience">
-            <p>Content for experience section</p>
+            <ul className="space-y-6">
+                {experiences.map(({ title, company, startDate, endDate }, index) => (
+                    <li 
+                        key={index} 
+                        className="flex flex-col md:flex-row md:items-center justify-between border-b-2 py-4"
+                    >
+                        <div className="font-medium text-lg">{title}</div>
+                        <div className="flex flex-col md:text-right md:items-end">
+                            <div className="text-secondary-foreground">{company}</div>
+                            <div className="text-sm text-secondary-foreground/50">{`${startDate} - ${endDate}`}</div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </Section>
     );
 }
--- src/components/partials/Section.jsx
+++ src/components/partials/Section.jsx
@@ -1,12 +1,12 @@
 import { SparklesIcon } from "lucide-react";
 export default function Section({ title, children }) {
     return (
-        <section className="py-16">
+        <section className="py-16 px-4 sm:px-8">
             <div className="container mx-auto flex items-center gap-2">
                 <SparklesIcon />
                 <h2 className="text-2xl font-bold text-foreground">{title}</h2>
             </div>
-            <div className="container mx-auto">{children}</div>
+            <div className="container mx-auto py-6">{children}</div>
         </section>
     );
 }
