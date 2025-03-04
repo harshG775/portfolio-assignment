53d31a894fddc0367a944ebb63c884550a097e7c
Harsh Gaur <hgaur491@gmail.com>
2025-03-04 21:44:39 +0530

    Section_Expertise

--- src/components/page/home/Section_Expertise.jsx
+++ src/components/page/home/Section_Expertise.jsx
@@ -1,9 +1,43 @@
 import Section from "@/components/partials/Section";
+import { CircleIcon } from "lucide-react";
+
+const expertise = [
+    {
+        title: "Branding",
+        description:
+            "I create efficient, adaptable, and engaging websites. No predefined patterns. No sluggish, complex code. Webflow forms the foundation of my web development approach. I employ it to provide safe, top-notch bespoke websites.",
+    },
+    {
+        title: "UI Design",
+        description:
+            "I create efficient, adaptable, and engaging websites. No predefined patterns. No sluggish, complex code. Webflow forms the foundation of my web development approach. I employ it to provide safe, top-notch bespoke websites.",
+    },
+    {
+        title: "UX Design",
+        description:
+            "I comprehend and resolve digital product issues using a user-focused methodology. Investigation, compassion, and visual conveyance are a few techniques I apply to captivate and involve your users while fulfilling your business requirements.",
+    },
+    {
+        title: "Development",
+        description:
+            "I create user-friendly, adaptive, engaging websites. No cookie-cutters. No cumbersome, complex coding. Webflow forms the foundation of my web development approach. I employ it to produce safe, top-notch personalized websites.",
+    },
+];
 
 export default function Section_Expertise() {
     return (
         <Section title="Expertise">
-            <p>Content for expertise section</p>
+            <ul className="grid gap-6 sm:grid-cols-2">
+                {expertise.map(({ title, description }, index) => (
+                    <li key={index} className="p-4 bg-muted rounded-xl shadow-sm">
+                        <div className="flex items-center gap-2">
+                            <CircleIcon className="w-4 h-4"/>
+                            <div className="font-semibold text-lg ">{title}</div>
+                        </div>
+                        <p className="text-sm md:text-base text-muted-foreground leading-relaxed mt-2">{description}</p>
+                    </li>
+                ))}
+            </ul>
         </Section>
     );
 }
--- src/components/partials/Section.jsx
+++ src/components/partials/Section.jsx
@@ -2,11 +2,11 @@ import { SparklesIcon } from "lucide-react";
 export default function Section({ title, children }) {
     return (
         <section className="py-16 px-4 sm:px-8">
-            <div className="container mx-auto flex items-center gap-2">
+            <div className="lg:max-w-7xl container mx-auto flex items-center gap-2">
                 <SparklesIcon />
                 <h2 className="text-2xl font-bold text-foreground">{title}</h2>
             </div>
-            <div className="container mx-auto py-6">{children}</div>
+            <div className="lg:max-w-7xl container mx-auto py-6">{children}</div>
         </section>
     );
 }
